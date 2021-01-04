f24a34de7c76a49240e8bb1c43d5be5a42e6429d
SOFIA <rusofiaru@gmail.com>
2021-01-05 01:11:16 +0200

    9.19: patientor, step4

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,47 @@
-export interface Entry {
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<DiagnoseEntry['code']>;
+}
+
+export type Entry =
+    | HospitalEntry
+    | OccupationalHealthcareEntry
+    | HealthCheckEntry;
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+interface Discharge {
+    date: string;
+    criteria: string;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: SickLeave
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: Discharge;
 }
 
 export enum Gender {
